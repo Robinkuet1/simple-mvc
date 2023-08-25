a68aaa3bee444b76997f2f716bfb142d970d2fc9
Robin Kuettel <robin.kuettel@roche.com>
2023-08-25 17:34:00 +0200

    .

--- main.py
+++ main.py
@@ -1,32 +1,32 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-# Sample data (you can replace this with a database)
-learning_times = []
-grades = []
+# model
+tasks = []
 
+
+# view
 @app.route('/')
 def index():
-    average_grade = calculate_average_grade()
-    return render_template('home.html', learning_times=learning_times, grades=grades, average_grade=average_grade)
+    return render_template('index.html', tasks=tasks)
 
-@app.route('/add_learning_time', methods=['POST'])
-def add_learning_time():
-    learning_time = int(request.form['learning_time'])
-    learning_times.append(learning_time)
-    return index()
 
-@app.route('/add_grade', methods=['POST'])
-def add_grade():
-    grade = int(request.form['grade'])
-    grades.append(grade)
-    return index()
+# controller
+@app.route('/add', methods=['POST'])
+def add_task():
+    title = request.form.get('title')
+    new_task = {'id': len(tasks) + 1, 'title': title, 'completed': False}
+    tasks.append(new_task)
+    return redirect(url_for('index'))
 
-def calculate_average_grade():
-    if not grades:
-        return 0
-    return sum(grades) / len(grades)
+@app.route('/complete/<int:task_id>')
+def complete_task(task_id):
+    for task in tasks:
+        if task['id'] == task_id:
+            task['completed'] = True
+            break
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
